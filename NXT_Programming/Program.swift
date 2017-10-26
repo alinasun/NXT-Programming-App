f9050873e98bd36d1a3ad021935397c24e09c8ab
Mangoxylic <erickcho@usc.edu>
2017-10-25 21:18:08 -0700

    Data persistence commit

---
+++ NXT_Programming/Program.swift
@@ -0,0 +1,13 @@
+//
+//  Program.swift
+//  NXT_Programming
+//
+//  Created by Erick Chong on 10/18/17.
+//  Copyright © 2017 LA's BEST. All rights reserved.
+//
+
+import UIKit
+
+class Program: NSObject {
+
+}
